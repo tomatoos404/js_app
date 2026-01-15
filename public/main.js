96338624a87bdcd9fdacaedfa2a355e4289f20c5
tomatoos404 <thomas.touny991@gmail.com>
2026-01-15 10:51:51 +0100

    correction

--- public/main.js
+++ public/main.js
@@ -1,19 +1,30 @@
+//
+// Récupération des éléments du DOM
 const monInput = document.getElementById('monInput');
 const monBouton = document.getElementById('monBouton');
+const monBouton2 = document.getElementById('monBouton2');
+ 
+// Ajout d'un écouteur d'événement sur le deuxième bouton
+monBouton2.addEventListener('click', () => {
+    fetch('/info').then(
+        response => response.json()
+    ).then(
+        JsonResponse =>     {
+            document.getElementById('reponse').innerHTML = JsonResponse.cle1;
+        }    
+    );   
+}); 
 
+// Ajout d'un écouteur d'événement sur le bouton
 monBouton.addEventListener('click', () => {
-    fetch('register', {
+    fetch('/register', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ data: monInput.value })
-    })
-    .then(response => response.text())
-    .then(data => {
-        alert(data);
-    })
-    .catch(error => {
-        console.error('Error:', error);
-    });
+        body: JSON.stringify({ inputValue: monInput.value })     
+    }).then(response => response.text())
+      .then(data => {
+          alert(data);
+      });
 });
--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const app = express();
+ 
 
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/login', (req, res) => {
-    res.send('Hello, World!');
+  res.send('<h1>Bienvenue sur la page de login  </h1>');
 });
+ 
 
+app.get('/info', (req, res) => {
+  res.json({ cle1: 'Toto', cle2: 'titi' });
+});
+ 
 
 app.post('/register', (req, res) => {
-    res.send('merci de manger votre chien');
+console.log('Données reçues pour l\'inscription');
+console.log(req.body);
+  res.json({ message: 'Inscription réussie !' });
 });
-
+ 
 
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+  let monIp = require("ip").address();
+  console.log(`Server running on http://${monIp}:3000`);
 });
-
